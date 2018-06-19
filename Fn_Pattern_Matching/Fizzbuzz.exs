d91481c482a212575792801674b35f7660e9afa9
Wanjiku Mac <edwardscaroline32@gmail.com>
2018-06-19 21:58:51 +0300

    fizzbuzz game

--- Fn_Pattern_Matching/Fizzbuzz.exs
+++ Fn_Pattern_Matching/Fizzbuzz.exs
@@ -5,9 +5,9 @@
 #Otherwise return the third argument.
 
 fizz_game = fn
-  (0,0,_) -> "FizzBuzz."
-  (0,_,_) -> "Fizz."
-  (_,0,_) -> "Buzz."
+  (0,0,_) -> "FizzBuzz"
+  (0,_,_) -> "Fizz"
+  (_,0,_) -> "Buzz"
   (_,_,c) -> "#{c}"
 end
  IO.puts fizz_game.(0,0,1)
